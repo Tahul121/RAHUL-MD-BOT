79b2da0fd0c618ceeee5b1c0fc765dbad486be18
Rahul Debnath <137095636+Tahul121@users.noreply.github.com>
2023-12-14 11:55:23 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,16 +6,16 @@ if (fs.existsSync('config.env')) require('dotenv').config({ path: __dirname+'/co
 global.owner = process.env.OWNER_NUMBER.split(",")
 global.mongodb = process.env.MONGODB_URI || "mongodb+srv://KING-MDBot: naveeddogar@cluster0.jkvj760.mongodb.net/?retryWrites=true&w=majority"
 global.port= process.env.PORT || 5000
-global.email = 'textnow1pak@gmail.com'
-global.github = 'https://github.com/naveeddogar/KING-MD'
-global.location = 'pakistan'
+global.email = 'rsstatus004@gmail.com'
+global.github = 'https://github.com/Tahul121/RAHUL-MD-BOT'
+global.location = 'india'
 global.gurl = 'https://instagram.com/naveeddogar_' // add your username
-global.sudo = process.env.SUDO || '923096566451'
-global.devs = '923096566451';
-global.website = 'https://github.com/naveeddogar/KING-MD' // Hello world
+global.sudo = process.env.SUDO || '919547261290'
+global.devs = '919547261290';
+global.website = 'https://github.com/Tahul121/RAHUL-MD-BOT' // Hello world
 global.THUMB_IMAGE = process.env.THUMB_IMAGE || 'https://telegra.ph/file/a6b9bbde7feaa92c69c7b.jpg'
 module.exports = {
-  botname:   process.env.BOT_NAME === undefined ? '𝙆𝙄𝙉𝙂-𝙈𝘿' : process.env.BOT_NAME,
+  botname:   process.env.BOT_NAME === undefined ? '𝑅𝛥𝛨𝑈𝐿-𝛭𝐷-𝐵𝛩𝑇' : process.env.BOT_NAME,
   ownername: process.env.OWNER_NAME === undefined ? 'Naveed Dogar' : process.env.OWNER_NAME,
   sessionName:  process.env.SESSION_ID === undefined ? false : process.env.SESSION_ID,
   author:  process.env.PACK_AUTHER.split(";")[0] === undefined ? 'Naveed-Dogar' : process.env.PACK_AUTHER.split(";")[0],
