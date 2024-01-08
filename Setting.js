100344a74d8290bb6da6bbc6c19d1c50abdae896
Rahul Debnath <137095636+Tahul121@users.noreply.github.com>
2024-01-08 18:42:20 +0530

    RAHUL-MD-BOT

--- Setting.js
+++ Setting.js
@@ -3,33 +3,33 @@ if (fs.existsSync('config.env')) require('dotenv').config({ path: __dirname+'/co
 
 
 //═══════[Required Variables]════════,[Make sure you put all values in "" , '']\\
-global.owner = process.env.OWNER_NUMBER || '923466319114' ; // Add Your Number without +
-global.mongodb = process.env.MONGODB_URI || "mongodb+srv://Maher-Zubair:SIGMA-MD@zubi.9g6b16y.mongodb.net/?retryWrites=true&w=majority&appName=AtlasApp" ; // put mongodb key here
+global.owner = process.env.OWNER_NUMBER || '919547261290' ; // Add Your Number without +
+global.mongodb = process.env.MONGODB_URI || "mongodb+srv://rahul:rahul@cluster0.ywtlw3t.mongodb.net/?retryWrites=true&w=majority" ; // put mongodb key here
 global.port=5000  ;
 global.audio = '' ; 
 global.video = '' ;
 global.blockJids = process.env.BLOCK_JID ||'120363169665426586@g.us' ;
 global.allowJids = process.env.ALLOW_JID ||'120363169665426586@g.us' ;
-global.email = 'HELP_SIGMA-MD@outlook.com' ; 
-global.github = 'https://github.com/Maher-Zubair/SIGMA-MD' ;
-global.location = 'Lahore Pakistan' ;
+global.email = 'rsstatus004@gmail.com' ; 
+global.github = 'https://github.com/Tahul121/RAHUL-MD-BOT' ;
+global.location = 'India' ;
 global.timezone  = process.env.TIME_ZONE || 'Asia/Karachi' //add correct timezone or leave it same , otherwise you get erros
-global.gurl = 'https://www.youtube.com/@InnoxentTech?sub_confirmation=1' ; 
-global.sudo =  process.env.SUDO || "923466319114" ; // Do not change it
-global.devs = "923466319114"; //Dont change it From here
-global.mztit = process.env.MZTIT ||"🅼♥︎❚❚♥︎🆉", // add your title here
-global.Gname = process.env.GNAME ||"sɪɢᴍᴀ ᴹᴰ-sᴜᴘᴘᴏʀᴛ",
-global.zyt = process.env.ZYT || 'https://www.youtube.com/@InnoxentTech?sub_confirmation=1',
-global.waUrl = process.env.WAURL ||"https://chat.whatsapp.com/CmY0THcJCUYEGxLJulhcRV",
-global.website = 'http://lnkiy.in/SIGMA-MD-WEB' ; 
-global.THUMB_IMAGE = process.env.THUMB_IMAGE || 'https://telegra.ph/file/03e49e6e2057568db8926.jpg' ;
+global.gurl = 'https://www.youtube.com/@rahultech009?sub_confirmation=1' ; 
+global.sudo =  process.env.SUDO || "919547261290" ; // Do not change it
+global.devs = "919547261290"; //Dont change it From here
+global.mztit = process.env.MZTIT ||"🆁🅰🅷🆄🅻", // add your title here
+global.Gname = process.env.GNAME ||"ʀᴀʜᴜʟ ᴹᴰ-sᴜᴘᴘᴏʀᴛ",
+global.zyt = process.env.ZYT || 'https://www.youtube.com/@rahultech009?sub_confirmation=1',
+global.waUrl = process.env.WAURL ||"https://chat.whatsapp.com/C7PFcSRmRI9Enbda8Cikqu",
+global.website = 'http://lnkiy.in/RAHUL-MD-WEB' ; 
+global.THUMB_IMAGE = process.env.THUMB_IMAGE || 'https://telegra.ph/file/ba7201b533cf2264f2237.jpg' ;
 module.exports = {
   sessionName: process.env.SESSION_ID || '',      //Put Your Session Id Here
-  botname: process.env.BOT_NAME || 'sɪɢᴍᴀ ᴹᴰ',  // add the botname you want
-  ownername:  process.env.OWNER_NAME || `ᴍᴀʜᴇʀ ᴢᴜʙᴀɪʀ`, // add your name
-  author:  process.env.PACK_AUTHER || 'ᴍᴀʜᴇʀ ᴢᴜʙᴀɪʀ', 
+  botname: process.env.BOT_NAME || 'ʀᴀʜᴜʟ ᴹᴰ',  // add the botname you want
+  ownername:  process.env.OWNER_NAME || `ʀᴀʜᴜʟ ᴅᴇʙɴᴀᴛʜ`, // add your name
+  author:  process.env.PACK_AUTHER || 'ʀᴀʜᴜʟ ᴅᴇʙɴᴀᴛʜ', 
   auto_read_status : process.env.AUTO_READ_STATUS || 'false', // if it true it automatically views status and send status in your inbox
-  packname:  process.env.PACK_NAME || "sɪɢᴍᴀ ᴹᴰ" , 
+  packname:  process.env.PACK_NAME || "ʀᴀʜᴜʟ ᴹᴰ" , 
   autoreaction: process.env.AUTO_REACTION || 'true', // if it true it will react to all messages
   antibadword : process.env.ANTI_BAD_WORD || 'nobadwordokey',
   alwaysonline: process.env.ALWAYS_ONLINE || 'true', // it will show always online, false it if you dont want
@@ -47,11 +47,11 @@ module.exports = {
   HEROKU_APP_NAME: process.env.HEROKU_APP_NAME,
   HEROKU_API_KEY: process.env.HEROKU_API_KEY,
   REMOVE_BG_KEY: process.env.REMOVE_BG_KEY || "34wcCGPoe3yaGtpiBZgx4SN7", // add your remove bg key if you have it
-  caption :process.env.CAPTION || "\t*⤹★ᴘᴏᴡᴇʀᴇᴅ ʙʏ★⤸ sɪɢᴍᴀ ᴹᴰ* ",   //*『sᴜʙsᴄʀɪʙᴇ • ɪɴɴᴏxᴇɴᴛ ᴛᴇᴄʜ』https://www.youtube.com/@InnoxentTech,
+  caption :process.env.CAPTION || "\t*⤹★ᴘᴏᴡᴇʀᴇᴅ ʙʏ★⤸ ʀᴀʜᴜʟ ᴹᴰ* ",   //*『sᴜʙsᴄʀɪʙᴇ • ʀᴀʜᴜʟ ᴛᴇᴄʜ』https://www.youtube.com/@rahultech009,
   promote_demote_messages : process.env.PROMOTE_DEMOTE_MESSAGES || 'true' ,
   OPENAI_API_KEY: process.env.OPENAI_API_KEY ||'' , // add your openai key if you have it
   VERSION: process.env.VERSION || '𝚅.𝟷.𝟸.𝟽',
-  LANG: process.env.THEME|| 'SIGMA_MD',
+  LANG: process.env.THEME|| 'RAHUL_MD',
   menu : process.env.MENU || '', // Not Available in current Version
   WORKTYPE: process.env.WORKTYPE || 'public' // if private only you can use your bot, if public everyone use your bot
 };
